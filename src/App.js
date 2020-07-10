3e7600819e35b08eedab31e348dc669c18d556f0
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-10 21:57:59 +0300

    Product list component created, layout added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header';
 import ContentForm from './components/ContentForm/ContentForm';
+import ProductList from './components/ProductList/ProductList';
 
 function App() {
     return (
         <React.Fragment>
             <Header/>
             <ContentForm/>
+            <ProductList/>
         </React.Fragment>
     );
 }
