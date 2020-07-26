ac1475e127c6a7a4213d86dbcacc791866b83a00
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-26 14:33:33 +0300

    Connect container Management

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/header';
 import {Footer} from './components/footer';
 import Products from './containers/Products';
+import Management from './containers/Management';
 
 function App() {
     return (
         <React.Fragment>
             <Header/>
+            <Management/>
             <Products/>
             <Footer/>
         </React.Fragment>
