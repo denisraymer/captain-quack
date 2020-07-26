712917e0b92b1af88079e0c17c08e111dff81942
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-26 13:12:46 +0300

    Create reducers

---
+++ src/reducers/managementReducer.js
@@ -0,0 +1,8 @@
+// import {CREATE_PRODUCT_SUCCESS} from '../actions/actionCreateProduct';
+
+export function managementReducer(state = {}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
