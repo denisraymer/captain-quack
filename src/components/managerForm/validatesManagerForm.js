342407cea1e2ff08549505b9707a326af26c4a60
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-27 12:19:25 +0300

    Changed validate function for text field

--- src/components/managerForm/validatesManagerForm.js
+++ src/components/managerForm/validatesManagerForm.js
@@ -1,5 +1,6 @@
 export const alphaNumeric = value => (value && /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(value) ? undefined : 'form-control-warning');
 
-export const required = value => value || typeof value === 'string' ? undefined : 'form-control-error';
+export const required = value => value || typeof value === 'string' ? undefined : 'Обязательно для заполнения';
 
-export const numeric = value => (value && /^[0-9]+$/.test(value) ? undefined : 'form-control-warning');
+export const numeric = value => (value && /^[0-9]+$/.test(value) ? undefined : 'Недопустимые символы');
+
