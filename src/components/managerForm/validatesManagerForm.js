44fb2f16509035cf9cee7c83dd55e102a72f5088
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-27 16:21:36 +0300

    Fixed error message for manager form

--- src/components/managerForm/validatesManagerForm.js
+++ src/components/managerForm/validatesManagerForm.js
@@ -1,4 +1,4 @@
-export const alphaNumeric = value => (value && /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(value) ? undefined : 'form-control-warning');
+export const alphaNumeric = value => (value && /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(value) ? undefined : 'Недопустимые символы');
 
 export const required = value => value || typeof value === 'string' ? undefined : 'Обязательно для заполнения';
 
