f18e09415046c2979b2dbd83457fba3f4d54b993
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-27 12:15:58 +0300

    Added title component

---
+++ src/components/title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Col, Container, Row} from 'react-bootstrap';
+
+export function Title() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <div className='title'>¡Ay caramba!</div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
